f732efd88aaaf2608764a552c72bb119f648937c
reeganbenny <reeganbenny@gmail.com>
2023-04-02 17:50:19 +0530

    Complete stage 1 for getting the number of players from the user

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const readlineSync = require("readline-sync");
-const { shuffleCards, initializePlayersPile } = require("./utils/helper");
-let drawPile = shuffleCards();
-console.log("** Card Game **");
-const numberOfPlayers = parseInt(
-  readlineSync.question("How many players are onboard? ")
-);
+const { startGame } = require("./controllers/startGame");
 
-({ playersPile, drawPile } = initializePlayersPile(numberOfPlayers, drawPile));
+let gameStatus = true;
+let numberOfPlayers = 0;
 
-console.log(playersPile.length);
-console.log(drawPile.length);
-// Chnage it to true when while loop is finished
-let gameStatus = false;
-while (gameStatus && drawPile.length !== 0) {
-  //
-}
+// Card Game initialization:- No. of players must be b/w 2 & 4 to start the game. If it's 0 Game stops!
+do {
+  console.clear();
+  numberOfPlayers !== 0
+    ? console.log("Number of players must be between 2 & 4 or Enter 0 to exit")
+    : console.log("\t\t\t** Swiggy Card Game **\n");
+  numberOfPlayers = parseInt(
+    readlineSync.question(
+      "How many players are onboard(0:exit, min:2 - max:4)? "
+    )
+  );
+  gameStatus = numberOfPlayers === 0 ? false : true;
+  if (numberOfPlayers >= 2 && numberOfPlayers <= 4) {
+    gameStatus = startGame(numberOfPlayers);
+  } else {
+    gameStatus && console.log("Exiting from game....");
+  }
+} while (numberOfPlayers !== 0 && gameStatus);
